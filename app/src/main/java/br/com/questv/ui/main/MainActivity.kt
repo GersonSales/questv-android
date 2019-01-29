9df9365b8a15cd406fdd518cf424b8d7d93672a3
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2019-01-29 00:38:24 -0300

    refact main activity

---
+++ app/src/main/java/br/com/questv/ui/main/MainActivity.kt
@@ -0,0 +1,15 @@
+package br.com.questv.ui.main
+
+import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+import android.support.v7.app.AppCompatActivity
+import br.com.questv.R
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+
+}
