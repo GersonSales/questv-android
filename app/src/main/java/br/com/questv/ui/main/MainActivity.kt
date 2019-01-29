b39eeed707b9ae764090b0ee18020f217322ed3f
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2019-01-29 01:51:45 -0300

    refact main

--- app/src/main/java/br/com/questv/ui/main/MainActivity.kt
+++ app/src/main/java/br/com/questv/ui/main/MainActivity.kt
@@ -1,38 +1,35 @@
 package br.com.questv.ui.main
 
 import android.os.Bundle
-import android.support.design.widget.TabLayout
-import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import br.com.questv.R
 import br.com.questv.ui.home.HomeFragment
 import br.com.questv.ui.main.fragments.Tab1
 import br.com.questv.ui.main.fragments.Tab2
 import br.com.questv.ui.main.fragments.Tab3
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 
-  private lateinit var tabAdapter: TabAdapter
-  private lateinit var tabLayout: TabLayout
-  private lateinit var viewPager: ViewPager
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    this.tabLayout = findViewById(R.id.tabLayout)
-    this.viewPager = findViewById(R.id.viewPager)
+    setupTabView()
+  }
 
-    this.tabAdapter = TabAdapter(supportFragmentManager)
+  private fun setupTabView() {
+    val tabAdapter = TabAdapter(supportFragmentManager)
 
-    this.tabAdapter.addFragment(HomeFragment(), "home")
-    this.tabAdapter.addFragment(Tab1(), "tab 1")
-    this.tabAdapter.addFragment(Tab2(), "tab 2")
-    this.tabAdapter.addFragment(Tab3(), "tab 3")
+    tabAdapter.addFragment(HomeFragment(), "home")
+    tabAdapter.addFragment(Tab1(), "tab 1")
+    tabAdapter.addFragment(Tab2(), "tab 2")
+    tabAdapter.addFragment(Tab3(), "tab 3")
 
-    this.viewPager.adapter = this.tabAdapter
-    this.tabLayout.setupWithViewPager(this.viewPager)
+    vp_main_pager.adapter = tabAdapter
+    tl_main_swiper.setupWithViewPager(vp_main_pager)
   }
 
 }
