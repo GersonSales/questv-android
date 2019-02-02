4c0c7a077187b5a7b49730d2d5c3261860b13127
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2019-02-02 19:02:26 -0300

    adjust series details layout

--- app/src/main/java/br/com/questv/ui/series/SeriesFragment.kt
+++ app/src/main/java/br/com/questv/ui/series/SeriesFragment.kt
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.*
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
@@ -15,6 +16,7 @@ import br.com.questv.model.season.SeasonModel
 import br.com.questv.model.series.SeriesModel
 import br.com.questv.resource.Strings.SERIES_KEY
 import br.com.questv.ui.home.HomeFragment
+import com.nostra13.universalimageloader.core.ImageLoader
 import kotlinx.android.synthetic.main.fragment_series.*
 
 class SeriesFragment : Fragment(), SeriesView {
@@ -28,18 +30,29 @@ class SeriesFragment : Fragment(), SeriesView {
     savedInstanceState: Bundle?
   ): View? {
     val view = inflater.inflate(R.layout.fragment_series, container, false)
-    val seriesName: TextView = view.findViewById(R.id.tv_series_details_name)
-
-    this.progressBar = view.findViewById(R.id.pb_season_recycler)
-
-
     val seriesModel: SeriesModel = arguments?.get(SERIES_KEY) as SeriesModel
-
-    seriesName.text = seriesModel.name
-    setHasOptionsMenu(true)
+    initView(view, seriesModel)
     this.presenter.fetchAllSeasons(seriesModel)
 
     return view
+  }
+
+  private fun initView(view: View?, seriesModel: SeriesModel) {
+    setHasOptionsMenu(true)
+    val seriesPromo: ImageView = view!!.findViewById(R.id.iv_series_details_promo)
+    val imageLoader = ImageLoader.getInstance()
+    val imageUrl = when {
+      !seriesModel.promoImageUrl!!.isEmpty() -> seriesModel.promoImageUrl
+      else -> seriesModel.coverImageUrl
+    }
+
+    imageLoader.displayImage(imageUrl!!.replace("localhost", "10.0.2.2"), seriesPromo)
+    seriesPromo.scaleType = ImageView.ScaleType.CENTER_CROP
+
+    val seriesName: TextView = view.findViewById(R.id.tv_series_details_name)
+    seriesName.text = seriesModel.name
+
+    this.progressBar = view.findViewById(R.id.pb_season_recycler)
   }
 
 
