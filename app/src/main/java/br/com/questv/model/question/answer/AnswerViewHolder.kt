f7bc82b00018ae57081e07cbda673299382913e2
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2019-02-03 07:58:10 -0300

    adjust question-answer layout

--- app/src/main/java/br/com/questv/model/question/answer/AnswerViewHolder.kt
+++ app/src/main/java/br/com/questv/model/question/answer/AnswerViewHolder.kt
@@ -1,5 +1,9 @@
 package br.com.questv.model.question.answer
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.support.constraint.ConstraintLayout
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
@@ -10,7 +14,13 @@ class AnswerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
   fun bind(answerModel: AnswerModel) {
     itemView.findViewById<TextView>(R.id.tv_answer_item_number)!!.text = answerModel.number.toString()
     itemView.findViewById<TextView>(R.id.tv_question_answer_item)!!.text = answerModel.description
+    val answerRow: ConstraintLayout = itemView.findViewById(R.id.cl_answer_row)
+    answerRow.setOnClickListener {
+      val colorCorrect = ContextCompat.getColor(itemView.context, R.color.colorGreen)
+      val colorWrong = ContextCompat.getColor(itemView.context, R.color.colorRed)
+      answerRow.setBackgroundColor(if (answerModel.isCorrect) colorCorrect else colorWrong)
 
+    }
   }
 
 }
