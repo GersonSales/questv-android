0f30a557df287aa93f26f11c838e17d6748b1882
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2019-03-23 02:38:28 -0300

    get start with profile layout

--- app/src/main/java/br/com/questv/ui/user/profile/UserProfile.kt
+++ app/src/main/java/br/com/questv/ui/user/profile/UserProfile.kt
@@ -5,15 +5,18 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import br.com.questv.R
 import br.com.questv.model.user.UserLocalStorage
+import kotlinx.android.synthetic.main.fragment_user_profile.*
 
 class UserProfile : Fragment() {
-  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-    val view = inflater.inflate(R.layout.fragment_user_profile, container, false)
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+    inflater.inflate(R.layout.fragment_user_profile, container, false)!!
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     val loggedUser = UserLocalStorage(context!!).getLoggedUserInfo()
-    view.findViewById<TextView>(R.id.tv_user_name).text = loggedUser.firstName
-    return view
+    tv_user_first_name.text = loggedUser.firstName
+    tv_user_last_name.text = loggedUser.lastName
+    super.onViewCreated(view, savedInstanceState)
   }
 }
